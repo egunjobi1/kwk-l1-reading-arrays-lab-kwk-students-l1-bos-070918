ca0f8c248203f5c93ad85cb8ebf5f8705610dec1
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-11 16:00:06 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -12,21 +12,21 @@ def first_student_by_index
 end
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  puts student_names[3]
 end
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+ puts student_names[5]
 end
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+ puts student_names.first
 end
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+  student_names.last
 end
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  student_names[0..2]
 end
